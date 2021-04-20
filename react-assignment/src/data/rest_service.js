ff927e724df00a0049bd518081eb62e1c3d22dfd
Mike0704 <haiduong7499@gmail.com>
2021-04-20 10:10:30 +0700

    Admin - Add Rest_Service folder for call product api

---
+++ react-assignment/src/data/rest_service.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const basehost = 'https://localhost:44303/';
+
+
+export const GetProduct = () => {
+    return axios.get(basehost + '/api/Product').then(response => response.data).catch((error) => {
+        console.log(error.response);
+        return [];
+    });
+};
