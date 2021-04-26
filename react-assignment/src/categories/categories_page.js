ba6f3fbf5dd1bcc2154f500e2c74ee8362a058bd
Mike0704 <haiduong7499@gmail.com>
2021-04-26 15:24:03 +0700

    admin-delete category

--- react-assignment/src/categories/categories_page.js
+++ react-assignment/src/categories/categories_page.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Button } from 'reactstrap';
-import {host} from '../config.js';
+import { host } from '../config.js';
+import {TrashFill} from 'react-bootstrap-icons';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Category = () => {
     const [categoryItems, setCategory] = useState([]);
 
+    const fetchCategoryData  = () => {
+        axios.get(host + "/api/Category")
+            .then(response => {
+                setCategory(response.data);
+            }).catch((error) => {
+                console.log('get category err', error);
+            });
+    }
     useEffect(() => {
-        axios.get(host +"/api/Category") 
-        .then(response => {
-            setCategory(response.data);
-        }).catch((error) => {
-            console.log('get category err', error);
-        });
+        fetchCategoryData();
     }, []);
 
+    const DeleteCategory = (id) => {
+        axios.delete(host + "/api/Category/" + id)
+            .then(response => {
+                fetchCategoryData();
+                setCategory(response.data);
+            }).catch((error) => {
+                console.log('get category err', error);
+            });
+    }
     return (
-        
+
         <>
             <h2 className="text-center p-3">Category</h2>
             <Button color="success" className='mb-2 ml-2'>
-            <Link className="text-decoration-none text-white"
+                <Link className="text-decoration-none text-white"
                     to='/createcategory_form'>
                     Create Category
                 </Link>
@@ -41,6 +54,11 @@ const Category = () => {
                                 <td>{category.categoryId}</td>
                                 <td>{category.name}</td>
                                 <td>{category.description}</td>
+                                <td>
+                                    <Button color="danger" className="mr-2" onClick={() => DeleteCategory(category.categoryId)}>
+                                        <TrashFill color="white" size={20} />
+                                    </Button>
+                                </td>
                             </tr>
                         )
                     }
