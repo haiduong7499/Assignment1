d4c176ebc250a1f92bd40d4a70a0d54e29376889
Mike0704 <haiduong7499@gmail.com>
2021-04-27 13:41:01 +0700

    get user

---
+++ Assignment.BackEnd/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Assignment.BackEnd.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<IdentityUser>> GetUser()
+        {
+            var user = await _context.Users.AsNoTracking().ToListAsync();
+            return user;
+        }
+    }
+}
